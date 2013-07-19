9561442a9943ccec9132267eb2a3de2b5b25df9c
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-07-19 13:03:31 -0500

    Breeze RA-115: changing varnames

--- backup_db.sh
+++ backup_db.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#MYSQLUSER=bamboouser
-MYSQLUSER=root
-MYSQLPW=$(<shh.txt)
-MYSQLSCHEMA=bamboo
-#BACKUPDIR='/opt/bamboo-home/backups'
-BACKUPDIR='.'
-CURRENTTIME=`date +"%Y-%m-%d-%H:%M"`
+bambooHome="/opt/bamboo-home"
+mysqlUser=bamboouser
+mysqlPw=$(<${bambooHome}/shh.txt)
+mysqlSchema=bamboo
+backupDir='/opt/bamboo-home/backups'
+currentTime=`date +"%Y-%m-%d-%H:%M"`
 
 checkSecret () {
   if [ ! -f shh.txt ]
@@ -23,7 +22,7 @@ dumpSchema () {
       echo 'The mysqldump command is not available.'
       exit;
     else
-      mysqldump -u ${MYSQLUSER} -p${MYSQLPW} ${MYSQLSCHEMA} | gzip -9 > ${BACKUPDIR}/bamboo_db_dump-${CURRENTTIME}.sql.gz
+      mysqldump -u ${mysqlUser} -p${mysqlPw} ${mysqlSchema} | gzip -9 > ${backupDir}/bamboo_db_dump-${currentTime}.sql.gz
   fi
 }
 
